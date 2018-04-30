16dc964b9befaaa30ed38702a56abf6a653b66a0
league <league@wts5.attlocal.net>
2018-04-30 16:17:14 -0700

    Finished Module 4

--- src/ChooseYourOwnAdventure.java
+++ src/ChooseYourOwnAdventure.java
@@ -7,9 +7,31 @@ public class ChooseYourOwnAdventure {
 	System.out.print(c1);
 	System.out.print(".");
 	System.out.println(" He wasn't a very normal "+ c1 + " and he had the ability to make birds fly.");
-	System.out.print(" At one point in his life, he decided to do something with his life. He wanted to become a ");
+	System.out.print("At one point in his life, he decided to do something with his life. He wanted to become a ");
 	String c2 = JOptionPane.showInputDialog("What did he want to become?");
-	System.out.print(c2);
+	System.out.print(c2 + ".");
+	System.out.println(" Once when he was walking down a street, he came upon a fork in the road.");
+	System.out.print("There were three ways which he could go down.");
+	int ch1 = JOptionPane.showOptionDialog(null, "Right or Left", "Choice 1", 0, JOptionPane.INFORMATION_MESSAGE, null,
+			new String[] { "Right", "Left", "Cliff" }, null);	
+	if(ch1 == 0) { //right
+		System.out.println("The "+ c1 + " went right and saw, that there was a giant see saw.");
+		System.out.print("For some reason, "+ c1 + " got on the see saw, and got killed by the giant who sat on him.");
+	}
+	else if(ch1 == 1) { //left
+		System.out.println("The "+ c1 + " went left, and kept going till dawn. He finally arrived at a town called Mawn");
+		System.out.println("And no, the programmer who made this didn't need a rhyme, it was just a good word for its time.");
+		System.out.println("When the "+ c1 + " arrived at the town, a ton of people seemed to celebrate his return. ");
+		System.out.println("For no good reason. Except...");
+		String r1 = JOptionPane.showInputDialog("What was the reason?");
+		System.out.println("For the reason of: "+ r1 );
+		System.out.println("And he actually did become a "+ c2 + ". Somehow.");
+		System.out.print("THE END");
+	}
+	else if(ch1 == 2) { //Cliff
+		System.out.println(" What a mighty mistake this fellow made, as falling off the cliff lead to his demise.");
+		System.out.print("This cliff had been enchanted with the falling spell, so anybody who fell off it would not live to tell.");
+	}
 	
 	}
 
--- src/HappyPet.java
+++ src/HappyPet.java
@@ -15,9 +15,25 @@ String pet = JOptionPane.showInputDialog("What kind of pet do you want to buy?")
 			// 3. Use showOptionDialog to ask the user what they want to do to make their pet happy
 			//    (eg: cuddle, food, water, take a walk, groom, clean up poop).
 			//    Make sure to customize the title and question too.
-for(int i = 0; i < 10; i++) {
+for(int i = 0; i < 5; i++) {
 	int task = JOptionPane.showOptionDialog(null, "What do you want to do to make your pet happy?", "Happy Pet", 0, JOptionPane.INFORMATION_MESSAGE, null,
 			new String[] { "Feed", "Take on a walk", "Lock in crate" }, null);	
+	if(task == 0) {
+		feed();
+	}
+	else if(task == 1) {
+		walk();
+	}
+	else if(task == 2) {
+		lock();
+	}
+	else if(task == -1) {
+		JOptionPane.showMessageDialog(null, "Why did you press that ?!");
+	}
+	if(happinessLevel == 3) {
+		JOptionPane.showMessageDialog(null, "You made your pet happy!");
+		break;
+	}
 }
 	}
 	// 5. Use user input to call the appropriate method created in step 4.
@@ -30,6 +46,7 @@ public static void feed() {
 		clip.open(AudioSystem.getAudioInputStream(HappyPet.class.getResource("HappyDog.wav")));
 		clip.start();
 		Thread.sleep(3600);
+		happinessLevel++;
 	} catch (Exception ex) {
 		ex.printStackTrace();
 	}
@@ -44,6 +61,7 @@ public static void walk() {
 		clip.open(AudioSystem.getAudioInputStream(HappyPet.class.getResource("HappyDog.wav")));
 		clip.start();
 		Thread.sleep(3600);
+		happinessLevel++;
 	} catch (Exception ex) {
 		ex.printStackTrace();
 	};
@@ -55,6 +73,7 @@ public static void lock() {
 		clip.open(AudioSystem.getAudioInputStream(HappyPet.class.getResource("MadDog.wav")));
 		clip.start();
 		Thread.sleep(3600);
+		happinessLevel++;
 	} catch (Exception ex) {
 		ex.printStackTrace();
 	}
